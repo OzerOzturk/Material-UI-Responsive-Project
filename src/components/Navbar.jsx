857a5ce9a41fb5ed0fb47e3f715efad7ad816fef
OzerOzturk <ozerozturk1636@gmail.com>
2022-03-14 23:59:34 +0300

    responsive design add.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,88 @@
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/styles";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  makeStyles,
+  InputBase,
+  Badge,
+  Avatar,
+} from "@material-ui/core";
+import { Mail, Notifications, Search } from "@material-ui/icons";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  logoLg: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  logoSm: {
+    display: "block",
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  search: {
+    display: "flex",
+    alignItems: "center",
+    background: "#8B8DD4",
+    "&:hover": {
+      background: "#BBBDF9",
+    },
+    borderRadius: theme.shape.borderRadius,
+    width: "40%",
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
+  },
+  input: {
+    color: "white",
+    marginLeft: theme.spacing(1),
+  },
+  searchButton: {
+    marginRight: theme.spacing(2),
+  },
+  icons: {
+    display: "flex",
+    alignItems: "center",
+  },
+  badge: {
+    marginRight: theme.spacing(2),
+  },
+}));
 
 function Navbar() {
   const classes = useStyles();
   return (
     <AppBar>
-      <Toolbar>
-        <Typography variant="h6">Virtue</Typography>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h6" className={classes.logoLg}>
+          Virtue Dev
+        </Typography>
+        <Typography variant="h6" className={classes.logoSm}>
+          Virtue
+        </Typography>
+        <div className={classes.search}>
+          <Search />
+          <InputBase placeholder="Search..." className={classes.input} />
+        </div>
+        <div className={classes.icons}>
+          <Search className={classes.searchButton} />
+          <Badge badgeContent={5} color="secondary" className={classes.badge}>
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color="secondary" className={classes.badge}>
+            <Notifications />
+          </Badge>
+          <Avatar
+            alt="Özer Öztürk"
+            src="https://avatars.githubusercontent.com/u/60944453?v=4"
+          />
+        </div>
       </Toolbar>
     </AppBar>
   );
