4471502a91b9fb85c8f124dd5fb4247c5a4d1232
OzerOzturk <ozerozturk1636@gmail.com>
2022-03-20 16:08:25 +0300

    create material UI Form.

--- src/components/Leftbar.jsx
+++ src/components/Leftbar.jsx
@@ -61,39 +61,39 @@ function Leftbar() {
       </div>
       <div className={classes.item}>
         <Person className={classes.icon} />
-        <Typography className={classes.text}>Homepage</Typography>
+        <Typography className={classes.text}>Friends</Typography>
       </div>
       <div className={classes.item}>
         <ListSharpIcon className={classes.icon} />
-        <Typography className={classes.text}>Homepage</Typography>
+        <Typography className={classes.text}>Lists</Typography>
       </div>
       <div className={classes.item}>
         <PhotoCamera className={classes.icon} />
-        <Typography className={classes.text}>Homepage</Typography>
+        <Typography className={classes.text}>Camera</Typography>
       </div>
       <div className={classes.item}>
         <PlayCircleOutline className={classes.icon} />
-        <Typography className={classes.text}>Homepage</Typography>
+        <Typography className={classes.text}>Videos</Typography>
       </div>
       <div className={classes.item}>
         <TabletMac className={classes.icon} />
-        <Typography className={classes.text}>Homepage</Typography>
+        <Typography className={classes.text}>Apps</Typography>
       </div>
       <div className={classes.item}>
         <Bookmark className={classes.icon} />
-        <Typography className={classes.text}>Homepage</Typography>
+        <Typography className={classes.text}>Collections</Typography>
       </div>
       <div className={classes.item}>
         <Storefront className={classes.icon} />
-        <Typography className={classes.text}>Homepage</Typography>
+        <Typography className={classes.text}>Market Place</Typography>
       </div>
       <div className={classes.item}>
         <Settings className={classes.icon} />
-        <Typography className={classes.text}>Homepage</Typography>
+        <Typography className={classes.text}>Settings</Typography>
       </div>
       <div className={classes.item}>
         <ExitToApp className={classes.icon} />
-        <Typography className={classes.text}>Homepage</Typography>
+        <Typography className={classes.text}>Logout</Typography>
       </div>
     </Container>
   );
