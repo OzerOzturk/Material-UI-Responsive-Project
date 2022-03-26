7831fa8639fea399962c591374b864c7119f4411
OzerOzturk <ozerozturk1636@gmail.com>
2022-03-26 13:50:56 +0300

    complete material UI Right Menu.

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,14 +1,124 @@
-import { Container, makeStyles } from "@material-ui/core";
+import {
+  Avatar,
+  Container,
+  Divider,
+  ImageList,
+  ImageListItem,
+  Link,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+import { AvatarGroup } from "@material-ui/lab";
 
 const useStyles = makeStyles((theme) => ({
   container: {
     paddingTop: theme.spacing(10),
+    position: "sticky",
+    top: 0,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 500,
+    color: "#555",
+  },
+  link: {
+    marginRight: theme.spacing(2),
+    color: "#555",
+    fontSize: 16,
   },
 }));
 
 function Rightbar() {
   const classes = useStyles();
-  return <Container className={classes.container}>rightbar</Container>;
+  return (
+    <Container className={classes.container}>
+      <Typography className={classes.title}>Online Friends</Typography>
+      <AvatarGroup max={6} style={{ marginBottom: 20 }}>
+        <Avatar
+          alt="Remy Sharp"
+          src="https://material-ui.com/static/images/avatar/1.jpg"
+        />
+        <Avatar
+          alt="Travis Howard"
+          src="https://material-ui.com/static/images/avatar/2.jpg"
+        />
+        <Avatar
+          alt="Cindy Baker"
+          src="https://material-ui.com/static/images/avatar/.jpg"
+        />
+        <Avatar
+          alt="Agnes Walker"
+          src="https://material-ui.com/static/images/avatar/4.jpg"
+        />
+        <Avatar
+          alt="Trevor Henderson"
+          src="https://material-ui.com/static/images/avatar/5.jpg"
+        />
+        <Avatar
+          alt="Trevor Henderson"
+          src="https://material-ui.com/static/images/avatar/6.jpg"
+        />
+        <Avatar
+          alt="Trevor Henderson"
+          src="https://material-ui.com/static/images/avatar/7.jpg"
+        />
+      </AvatarGroup>
+      <Typography className={classes.title}>Gallery</Typography>
+      <ImageList
+        rowHeight={140}
+        className={classes.imageList}
+        style={{ marginBottom: 20 }}
+        cols={2}
+      >
+        <ImageListItem>
+          <img
+            src="https://v4.mui.com/static/images/image-list/morning.jpg"
+            alt=""
+          />
+        </ImageListItem>
+        <ImageListItem>
+          <img
+            src="https://v4.mui.com/static/images/image-list/honey.jpg"
+            alt=""
+          />
+        </ImageListItem>
+        <ImageListItem>
+          <img
+            src="https://v4.mui.com/static/images/image-list/camera.jpg"
+            alt=""
+          />
+        </ImageListItem>
+        <ImageListItem>
+          <img
+            src="https://v4.mui.com/static/images/image-list/bike.jpg"
+            alt=""
+          />
+        </ImageListItem>
+        <ImageListItem>
+          <img
+            src="https://v4.mui.com/static/images/image-list/olive.jpg"
+            alt=""
+          />
+        </ImageListItem>
+      </ImageList>
+      <Typography className={classes.title} gutterBottom>
+        For More Browse Below 🔍
+      </Typography>
+      <Link href="#" className={classes.link} variant="body2">
+        Sport
+      </Link>
+      <Link href="#" className={classes.link} variant="body2">
+        Food
+      </Link>
+      <Divider flexItem style={{ marginBottom: 10 }} />
+      <Link href="#" className={classes.link} variant="body2">
+        Movies
+      </Link>
+      <Link href="#" className={classes.link} variant="body2">
+        Science
+      </Link>
+    </Container>
+  );
 }
 
 export default Rightbar;
